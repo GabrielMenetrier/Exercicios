6a1b2bfcd7fef64440df42c2d8f0565b92fa1712
Gabriel Renato Menetrier Santi <gabriel.r.m.santi@gmail.com>
2024-08-21 12:00:57 -0300

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,18 @@
+def integral(f,a,b,dx=0.001):
+    if a>b:
+        return 0
+    else:
+        return dx*f(a) + integral(f,a+dx,b,dx=0.001)
+    
+def integral2(f,a,b,dx=0.001):
+    z = 0
+    i = 0
+    while a+i*dx < b:
+        z = z + dx*f(a+i*dx)
+        i = i+1
+    return z
+
+
+
+print(integral(lambda x: x+x**4+35, 0, 1, 0.01))
+print(integral2(lambda x: x+x**4+35, 0, 1,0.01))
