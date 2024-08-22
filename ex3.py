8cb8b0c3e3dfea8389164ecf35b93f98601ed723
Gabriel Renato Menetrier Santi <gabriel.r.m.santi@gmail.com>
2024-08-22 12:05:35 -0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -11,7 +11,14 @@ def collatz_steps(k):
         return 3 + collatz_steps(collatz_next((3*k+1)//2))
     else:
         return 1 + collatz_steps(collatz_next(k))
-    
+
+def while_collatz_steps(k):
+        z = 0
+        while k != 1:
+            k = collatz_next(k)
+            z = z + 1
+        return z
+
 def collatz_steps_std(x):
     if x == 1:
         return 0
@@ -21,3 +28,4 @@ def collatz_steps_std(x):
 n = int(input("Digite um número: "))
 print(collatz_steps(n))
 print(collatz_steps_std(n))
+print(while_collatz_steps(n))
